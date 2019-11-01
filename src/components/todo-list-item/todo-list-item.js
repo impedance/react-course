e06267a50e018072928db734c4da3891b0836495
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2019-11-01 17:38:47 +0300

    new

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,23 +4,35 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  state = {
+    done: false,
+    important: false
+  } 
+
   onLabelClick = () => {
-      console.log(this.props.label);
+      this.setState({
+        done: true
+      })
   }
 
   render() {
-    const { label, important = false } = this.props;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    let classNames = 'todo-list-item';
+
+    if (done) {
+      classNames = `${classNames} done`
+    }
+
+    if (important) {
+      classNames = `${classNames} important`
+    }
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.onLabelClick}>
           {label}
         </span>
